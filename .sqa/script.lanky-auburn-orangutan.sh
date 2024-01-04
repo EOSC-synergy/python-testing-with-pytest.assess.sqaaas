6f957000d990eda245beb464755f4ad1a15f0e47
orviz <orviz@ifca.unican.es>
2024-01-04 13:22:39 +0100

    Add JePL file structure

---
+++ .sqa/script.lanky-auburn-orangutan.sh
@@ -0,0 +1,8 @@
+# SPDX-FileCopyrightText: Copyright contributors to the Software Quality Assurance as a Service (SQAaaS) project <sqaaas@ibergrid.eu>
+#
+# SPDX-License-Identifier: GPL-3.0-only
+
+(
+cd github.com/jashburn8020/python-testing-with-pytest &&
+    get_git_tags.py
+)
---
+++ .sqa/script.pasty-maroon-bandicoot.sh
@@ -0,0 +1,8 @@
+# SPDX-FileCopyrightText: Copyright contributors to the Software Quality Assurance as a Service (SQAaaS) project <sqaaas@ibergrid.eu>
+#
+# SPDX-License-Identifier: GPL-3.0-only
+
+(
+cd github.com/jashburn8020/python-testing-with-pytest &&
+    find_doc_files.py --file_type all
+)
